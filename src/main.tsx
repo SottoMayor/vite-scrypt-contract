ff3776089a5c8c9e3839c3765e58b4fbb31de125
SottoMayor <ddsmmf@gmail.com>
2025-11-12 21:51:25 -0400

    Loading the new HelloWorld contract

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { Helloworld } from './contracts/helloWorldBsvContract.ts'
-import artifact from '../artifacts/helloWorldBsvContract.json'
-Helloworld.loadArtifact(artifact);
+import { HelloworldNew } from './contracts/HelloWorldNew.ts'
+import artifact from '../artifacts/HelloWorldNew.json'
+HelloworldNew.loadArtifact(artifact);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
